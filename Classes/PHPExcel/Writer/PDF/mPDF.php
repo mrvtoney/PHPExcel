8cba875590b2753753ed0ac1a98e5c875770553a
mrvtoney <mrvtoney@gmail.com>
2020-03-20 13:10:39 -0700

    mpdf 8.0.5 modfications
    Updated so that mpdf 8.05 modifications work.

--- Classes/PHPExcel/Writer/PDF/mPDF.php
+++ Classes/PHPExcel/Writer/PDF/mPDF.php
@@ -1,7 +1,7 @@
 <?php
 
 /**  Require mPDF library */
-$pdfRendererClassFile = PHPExcel_Settings::getPdfRendererPath() . '/mpdf.php';
+$pdfRendererClassFile = PHPExcel_Settings::getPdfRendererPath() . 'Mpdf.php';
 if (file_exists($pdfRendererClassFile)) {
     require_once $pdfRendererClassFile;
 } else {
@@ -91,7 +91,7 @@ class PHPExcel_Writer_PDF_mPDF extends PHPExcel_Writer_PDF_Core implements PHPEx
 
 
         //  Create PDF
-        $pdf = new mpdf();
+        $pdf = new Mpdf\Mpdf();
         $ortmp = $orientation;
         $pdf->_setPageSize(strtoupper($paperSize), $ortmp);
         $pdf->DefOrientation = $orientation;
